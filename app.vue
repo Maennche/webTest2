35ed4e00b83876da2dc72a1e12f6844865093a36
Matthew Maennche <matthew@maennche.com>
2025-01-03 13:06:41 -0600

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,39 +1,5 @@
 <template>
   <div>
-    <UHeader>
-      <template #logo>
-        <span>
-          Demo Studio
-        </span>
-      </template>
-      <template #right>
-        <UColorModeButton />
-      </template>
-    </UHeader>
-    <UMain>
-      <NuxtPage />
-    </UMain>
-    <UFooter>
-        <template #right>
-          <div class="flex items-center justify-center gap-4">
-            <UButton
-              icon="i-mdi-github"
-              :to="'https:github.com'"
-              color="gray"
-              variant="ghost"
-              target="_blank"
-              :padded="false"
-            />
-            <UButton
-              icon="i-mdi-twitter"
-              :to="'https:twitter.com'"
-              color="gray"
-              variant="ghost"
-              target="_blank"
-              :padded="false"
-            />
-          </div>
-        </template>
-      </UFooter>
+    <NuxtPage />
   </div>
 </template>
