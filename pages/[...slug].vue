b1ab9cf61331a0deaf1f7481b3a171272efd32a1
Matthew Maennche <matthew@maennche.com>
2025-01-03 12:33:19 -0600

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,19 @@
 <template>
-  <main>
-    <ContentDoc />
-  </main>
+  <UContainer>
+    <UPage>
+      <UPageHeader :title="page.title" :description="page.description" />
+      <UPageBody prose>
+        <ContentRenderer v-if="page.body" :value="page" />
+      </UPageBody>
+    </UPage>
+  </UContainer>
 </template>
+
+<script setup lang="ts">
+  const route = useRoute()
+
+  const { date: page } = await useAsncData(route.path, () => queryContent{route.path).findOne())
+  if (!page.value) {
+    throw createError({statusCode 404, statusMessage: 'Page not found'})
+  }
+</script>
